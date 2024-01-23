7e4ecc83bd5e76ee66a8e2651ede9f0e21985640
2jzAlan <10531.jpg@gmail.com>
2024-01-23 07:04:10 -0500

    added navbar but missing icons

---
+++ MoodBeat/src/components/NavBar.js
@@ -0,0 +1,47 @@
+// NavBar.js
+
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+
+const NavBar = () => {
+  return (
+    <View style={styles.navBar}>
+      <TouchableOpacity style={styles.navItem}>
+        <Text style={styles.navText}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navItem}>
+        <Text style={styles.navText}>Create</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navItem}>
+        <Text style={styles.navText}>Search</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navItem}>
+        <Text style={styles.navText}>Profile</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navBar: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    height: 71,
+    width: "100%",
+    borderTopWidth: 1,
+    borderTopColor: "#909090"
+  },
+  navItem: {
+    flex: 1,
+    alignItems: "center",
+    paddingVertical: 10,
+    backgroundColor: "#26282C",
+    borderRadius: 5,
+  },
+  navText: {
+    color: "#909090",
+    fontSize: 16,
+  },
+});
+
+export default NavBar;
