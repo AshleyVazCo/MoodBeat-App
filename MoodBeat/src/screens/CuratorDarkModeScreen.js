692c7a09bd4f521220e8a8513e5a30f74b5e5a4f
2jzAlan <10531.jpg@gmail.com>
2024-01-23 07:04:45 -0500

    added navbar, and cards to CDMS

--- MoodBeat/src/screens/CuratorDarkModeScreen.js
+++ MoodBeat/src/screens/CuratorDarkModeScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, TouchableHighlight, ScrollView } from "react-native";
 import { useFonts, BarlowCondensed_400Regular } from '@expo-google-fonts/barlow-condensed';
 import ShareSettingHeader from "../components/ShareSettingHeader";
+import MoodBoardCard from "../components/MoodBoardCard";
+import NavBar from "../components/NavBar";
 
 const CuratorDarkModeScreen = ({ navigation }) => {
   const [fontsLoaded] = useFonts({
@@ -18,16 +20,31 @@ const CuratorDarkModeScreen = ({ navigation }) => {
       <View style={styles.profile}>
         <Text style={styles.imageText}>Image</Text>
       </View>
-        <View style={styles.tabMenu}>
-            <Button
-                onPress={() => navigation.navigate('Setting')}
-                title="Created"
-            />
-            <Button
-                onPress={() => navigation.navigate('Setting')}
-                title="Saved"
-            />
-        </View>
+      <View style={styles.tabMenu}>
+        <TouchableHighlight
+          onPress={() => navigation.navigate('Setting')}
+          style={[styles.buttonContainer, { backgroundColor: "#4F4F4F" }]}
+        >
+          <Text style={[styles.buttonText, { color: "#CA9CE1" }]}>Created</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => navigation.navigate('Setting')}
+          style={[styles.buttonContainer, { backgroundColor: "#161717" }]}
+        >
+          <Text style={[styles.buttonText, { color: "#909090" }]}>Saved</Text>
+        </TouchableHighlight>
+      </View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.cardContainer}>
+        <MoodBoardCard
+          imageSource="https://example.com/image.jpg"
+          title="Card Title 1"
+        />
+        <MoodBoardCard
+          imageSource="https://example.com/image2.jpg"
+          title="Card Title 2"
+        />
+          </ScrollView>
+          <NavBar />
     </View>
   );
 };
@@ -43,6 +60,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  tabMenu: {
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 20,
+  },
+  buttonContainer: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontFamily: "BarlowCondensed_400Regular",
+    fontSize: 17,
+  },
+  cardContainer: {
+    marginLeft: 8,
+  },
 });
 
 export default CuratorDarkModeScreen;
+
