d85886e736083ebfc4968dd96d3ae55c4d85ca41
AshleyVazCo <Ashley.vazquezcolon@gmail.com>
2024-01-31 14:28:28 -0500

    Updated App.js to link my screens

--- MoodBeat/App.js
+++ MoodBeat/App.js
@@ -4,6 +4,10 @@ import CuratorDMScreen from './src/screens/CuratorDMScreen';
 import CuratorSelectionDMScreen from './src/screens/CuratorSelectionDMScreen';
 import CuratorLMScreen from './src/screens/CuratorLMScreen';
 import CuratorSelectionLMScreen from './src/screens/CuratorSelectionLMScreen';
+import ProfileScreenDM from './src/screens/ProfileScreenDM';
+import ProfileScreenLM from './src/screens/ProfileScreenLM';
+import ProfileSectionDM from './src/screens/ProfileSectionDM';
+import ProfileSectionLM from './src/screens/ProfileSectionLM';
 
 const Stack = createStackNavigator();
 
@@ -29,6 +33,26 @@ const App = () => {
           <Stack.Screen
           name="CuratorSelectionLM"
           component={CuratorSelectionLMScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ProfileScreenDM"
+          component={ProfileScreenDM}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ProfileScreenLM"
+          component={ProfileScreenLM}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ProfileSectionDM"
+          component={ProfileSectionDM}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ProfileSectionLM"
+          component={ProfileSectionLM}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
