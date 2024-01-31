aebdfaa9116699fd507fb375e866821599dffa2a
2jzAlan <10531.jpg@gmail.com>
2024-01-31 10:30:08 -0500

    add modified CSLM

--- MoodBeat/src/screens/CuratorSelectionLMScreen.js
+++ MoodBeat/src/screens/CuratorSelectionLMScreen.js
@@ -14,7 +14,6 @@ const CuratorSelectionLMScreen = ({ navigation }) => {
       imageSource: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "My Love Mine all mine",
     },
-    // Add more songs as needed
   ];
 
   const renderItem = ({ item }) => (
