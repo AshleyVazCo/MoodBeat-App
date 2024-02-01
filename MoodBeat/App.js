71c93c911d0386f49615c4a8108996f007f158e2
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2024-02-01 18:15:23 -0500

    worked on LoginModal as well as set up Sign Up Modal

--- MoodBeat/App.js
+++ MoodBeat/App.js
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="CuratorDM">
+      <Stack.Navigator initialRouteName="LoginScreenLM">
         <Stack.Screen
           name="CuratorDM"
           component={CuratorDMScreen}
