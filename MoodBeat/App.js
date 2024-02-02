90dafb9bbc75c1ab74f35440fb2005c56536c59e
VibewithZari <azarieac@knights.ucf.edu>
2024-02-02 00:11:23 -0500

    Fixed the missing screens in App.js

--- MoodBeat/App.js
+++ MoodBeat/App.js
@@ -12,6 +12,9 @@ import ProfileSectionLM from "./src/screens/ProfileSectionLM";
 import CreationScreenLM from './src/screens/CreationScreenLM';
 import CreationScreenDM from './src/screens/CreationScreenDM';
 import HomeLMScreen from './src/screens/HomeLMScreen';
+import HomeDMScreen from './src/screens/HomeDMScreen';
+import SearchDMScreen from './src/screens/SearchDMScreen';
+import SearchLMScreen from "./src/screens/SearchLMScreen";
 
 
 const Stack = createStackNavigator();
@@ -65,8 +68,23 @@ const App = () => {
           options={{ headerShown: false }}
         />
         <Stack.Screen
+          name="SearchLM"
+          component={SearchLMScreen}
+          options={{ headerShown: false }}
+          />
+          <Stack.Screen
+          name="HomeDM"
+          component={HomeDMScreen}
+          options={{ headerShown: false }}
+          />
+         <Stack.Screen
           name="CreationDM"
           component={CreationScreenDM}
+          options={{ headerShown: false }}
+          />
+         <Stack.Screen
+          name="SearchDM"
+          component={SearchDMScreen}
           options={{ headerShown: false }}
           />
           <Stack.Screen
