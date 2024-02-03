89982a7c5d2012217d7757b565dae32bc0b8ca61
VibewithZari <azarieac@knights.ucf.edu>
2024-02-02 22:01:45 -0500

    Fixing up the App.js file again due to misunderstanding

--- MoodBeat/App.js
+++ MoodBeat/App.js
@@ -5,77 +5,107 @@ import CuratorSelectionDMScreen from "./src/screens/CuratorSelectionDMScreen";
 import CuratorLMScreen from "./src/screens/CuratorLMScreen";
 import CuratorSelectionLMScreen from "./src/screens/CuratorSelectionLMScreen";
 import LoginScreenLM from "./src/screens/LoginScreenLM";
+import LoginScreenDM from "./src/screens/loginScreenDM";
 import ProfileScreenDM from "./src/screens/ProfileScreenDM";
 import ProfileScreenLM from "./src/screens/ProfileScreenLM";
 import ProfileSectionDM from "./src/screens/ProfileSectionDM";
 import ProfileSectionLM from "./src/screens/ProfileSectionLM";
-import CreationScreenLM from './src/screens/CreationScreenLM';
-import CreationScreenDM from './src/screens/CreationScreenDM';
+import CreationScreenLM from "./src/screens/CreationScreenLM";
+import CreationScreenDM from "./src/screens/CreationScreenDM";
+import HomeDMScreen from "./src/screens/HomeDMScreen";
+import HomeLMScreen from "./src/screens/HomeLMScreen";
+import SearchDMScreen from "./src/screens/SearchDMScreen";
+import SearchLMScreen from "./src/screens/SearchLMScreen";
 
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="CuratorLM">
-        <Stack.Screen
-          name="CuratorDM"
-          component={CuratorDMScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="CuratorSelectionDM"
-          component={CuratorSelectionDMScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="CuratorLM"
-          component={CuratorLMScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="CuratorSelectionLM"
-          component={CuratorSelectionLMScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ProfileScreenDM"
-          component={ProfileScreenDM}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ProfileScreenLM"
-          component={ProfileScreenLM}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ProfileSectionDM"
-          component={ProfileSectionDM}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="ProfileSectionLM" 
-        component={ProfileSectionLM} 
-        options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="CreationLM"
-          component={CreationScreenLM}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="CreationDM"
-          component={CreationScreenDM}
-          options={{ headerShown: false }}
-          />
-          <Stack.Screen
-          name="LoginScreenLM"
-          component={LoginScreenLM}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+ return (
+  <NavigationContainer>
+   <Stack.Navigator initialRouteName="CuratorLM">
+    <Stack.Screen
+     name="CuratorDM"
+     component={CuratorDMScreen}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="CuratorSelectionDM"
+     component={CuratorSelectionDMScreen}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="CuratorLM"
+     component={CuratorLMScreen}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="CuratorSelectionLM"
+     component={CuratorSelectionLMScreen}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="ProfileScreenDM"
+     component={ProfileScreenDM}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="ProfileScreenLM"
+     component={ProfileScreenLM}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="ProfileSectionDM"
+     component={ProfileSectionDM}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen name="ProfileSectionLM"
+    component={ProfileSectionLM}
+    options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="CreationLM"
+     component={CreationScreenLM}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="CreationDM"
+     component={CreationScreenDM}
+     options={{ headerShown: false }}
+     />
+    <Stack.Screen
+     name="LoginScreenLM"
+     component={LoginScreenLM}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="LoginScreenDM"
+     component={LoginScreenDM}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="HomeDM"
+     component={HomeDMScreen}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="HomeLM"
+     component={HomeLMScreen}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="SearchDM"
+     component={SearchDMScreen}
+     options={{ headerShown: false }}
+    />
+    <Stack.Screen
+     name="SearchLM"
+     component={SearchLMScreen}
+     options={{ headerShown: false }}
+    />
+   </Stack.Navigator>
+  </NavigationContainer>
+ );
 };
 
 export default App;
