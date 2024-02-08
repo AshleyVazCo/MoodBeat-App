11d30827ec40c28f047c18f62446b25afe32b713
2jzAlan <10531.jpg@gmail.com>
2024-02-08 08:59:05 -0500

    add verificationScreenLM

--- MoodBeat/src/components/VerificationScreenLM.js
+++ MoodBeat/src/components/VerificationScreenLM.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, TextInput } from 'react-native';
 
 const VerificationScreenLM = ({ onClose }) => {
     return (
@@ -13,6 +13,13 @@ const VerificationScreenLM = ({ onClose }) => {
                         style={styles.image} source={require('../../assets/icons/logoPurple.png')}
                     />
                 </View>
+            <View style={styles.verifyTitle}>
+              <Text style={styles.loginTitleText}>Verify your Information</Text>
+                </View>
+                <View style={styles.formContainer}>
+              <TextInput placeholder="Phone Number" style={styles.input} placeholderTextColor={'black'} />
+              <TextInput placeholder="Email" secureTextEntry style={styles.input} placeholderTextColor={'black'} />
+            </View>
             </View>
     </View>
   );
@@ -34,18 +41,42 @@ const styles = StyleSheet.create({
     height: 30,
     resizeMode: 'cover',
     },
-      modalContainer: {
+modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
-        modalContent: {
+modalContent: {
     height: 760,
     width: 380,
     backgroundColor: '#FFFFFC',
     padding: 20,
     borderRadius: 10,
+    },
+verifyTitle: {
+    justifyContent: 'center',
+    top: 20,
+  },
+  loginTitleText: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontFamily: "BarlowCondensed_400Regular",
+    color: '#43357A',
+    fontSize: 30,
+    },
+input: {
+    borderRadius: 10,
+    width: '80%',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    },
+formContainer: {
+    justifyContent: 'center',
+    top: 160,
+    left: 25
   },
 });
 
